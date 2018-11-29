bff8415aa1fad1f68e1735759867d1a922194da8
Juan Carlos Hernandez Ramos <jc.hernandez129@uniandes.edu.co>
2018-11-29 21:12:47 +0100

    Crup mothersihips

--- sigem-client/src/components/HelloWorld.vue
+++ sigem-client/src/components/HelloWorld.vue
@@ -1,45 +1,81 @@
 <template>
-  <div class="hello">
+  <div class='hello'>
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <p></p>
+    <h3>Add ships to system</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li>
+        <span>shipName is: {{ shipName }}</span>
+        <br>
+        <input name="shipName" v-model="shipName" type="text" placeholder="New ship name">
+        <span class="error" v-show="errors['shipName']">{{ errors["shipName"] }}</span>
+        <br>
+        <button @click="save()">Save</button>
+        <div v-for="item in motherships" :key="item.id">
+          {{item.id}} , {{item.name}}
+        </div>
+      </li>
     </ul>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    ...mapGetters('mothership', { findMothershipInStore: 'find' }),
+    motherships () {
+      const motherships = this.findMothershipInStore({
+        query: {
+          $sort: {createdAt: -1}
+        }
+      })
+      return motherships && motherships.data ? motherships.data : []
+    }
+  },
+  data() {
+    return {
+      shipName: null,
+      errors: {}
+    }
+  },
+  methods: {
+    ...mapActions('mothership', { findMotherships: 'find' }),
+    ...mapActions('mothership', { createMothership: 'create' }),
+    save() {
+      if (this.validateForm()) {
+        this.createMothership({
+          name: this.shipName
+        }).then(() => {
+          this.shipName = ''
+        }).catch(err => {
+          this.errors = {}
+          this.errors['shipName'] = err
+        })
+      }
+    },
+    validateForm() {
+      this.errors = {}
+      if (!this.shipName) {
+        this.errors['shipName'] = 'ship name is required'
+      }
+      return Object.keys(this.errors).length === 0
+    }
+  },
+  created() {
+    this.findMotherships().then(data => {
+      console.log(data)
+    }).catch(error => console.log(error))
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
@@ -53,6 +89,9 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #301c7a;
+}
+error {
+  color: red;
 }
 </style>
--- sigem-client/src/services/index.js
+++ sigem-client/src/services/index.js
@@ -2,7 +2,6 @@ import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
 import io from 'socket.io-client'
 
-console.log(process.env.VUE_APP_BASE_API_URL)
 const socket = io(process.env.VUE_APP_BASE_API_URL, {transports: ['websocket']})
 
 const feathersClient = feathers()
